05a4d324cca2d0c97ed9d99f08979db085a499bc
happy-spark <107300540+happy-spark@users.noreply.github.com>
2024-04-02 00:15:34 +0900

    코드 효율성 향상

--- script.py
+++ script.py
@@ -1,6 +1,21 @@
 import sys
 sys.set_int_max_str_digits(2147483647)
 
+def eratosthenes_sieve(limit):
+    limitn = limit+1
+    primes = dict()
+    for ind, val in enumerate([True] * limitn): primes[ind] = val
+
+    primes[0] = primes[1] = False
+
+    for ind, val in enumerate(primes):
+        if val is True:
+            primes[ind*2::ind] = [False] * (((limit - ind)//ind) - 1)
+    return primes
+
+limit = pow(2, 37)
+primes = eratosthenes_sieve(limit)
+
 c = 1
 z=0
 T=0
@@ -8,17 +23,9 @@ mo=0
 ja=0
 wow=0
 mom=0
-def is_prime(number):
-    if number < 2:
-        return 0
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            return 0
-    return 1
 
-
-while c<=pow(2,37):
-    if is_prime(c)==1 and is_prime(2*c-1) == 1 :
+while c<=limit:
+    if primes[c] and primes[2*c-1]:
         m=(pow(3,c-1)-1)%(2*c-1)
         n=(pow(3,c-1)+1)%(2*c-1)
         
